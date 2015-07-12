df3d479dcb9fb70e664141a65db7a22f79aaf341
greywolf <grey@gr3yw0lf.com>
2015-07-12 15:56:22 -0400

    Fix for directory/volume, readability of start.sh

--- assets/start.sh
+++ assets/start.sh
@@ -2,7 +2,11 @@
 
 /opt/docker-openvpn/bin/mknodTun.sh
 
-/usr/sbin/openvpn-nl --writepid /var/run/openvpn-nl.server.pid --daemon ovpn-server --cd /etc/openvpn-nl --config /etc/openvpn-nl/server.conf
+/usr/sbin/openvpn-nl --writepid /var/run/openvpn-nl.server.pid \
+		--daemon ovpn-server \
+		--cd /etc/openvpn-nl \
+		--config /etc/openvpn-nl/server.conf \
+		--server $OPENVPN_NETWORK $OPENVPN_NETMASK
 
 date > /var/log/openvpn/start.log
 # Forces the docker to stay running
