e466efe5f8f68177bb0827c6a50605a5747562a9
greywolf <grey@gr3yw0lf.com>
2015-07-28 15:17:58 -0400

    Adding openvpn pam files

--- assets/start.sh
+++ assets/start.sh
@@ -17,6 +17,9 @@ if [ "$1" == "openvpn" ]; then
 	exit;
 fi
 if [ "$1" == "openvpn-authpam" ]; then
+	# bring PAM config active
+	cp /opt/docker-openvpn/bin/openvpn-auth-pam /etc/pam.d/openvpn
+
 	/usr/sbin/openvpn-nl --writepid /var/run/openvpn-nl.server.pid \
 		--daemon ovpn-server \
 		--cd /etc/openvpn-nl \
